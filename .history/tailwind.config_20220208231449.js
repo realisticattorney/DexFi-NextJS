07e2a31113d47500bacd4151eb277aaa29a1992e
realisticattorney <g.axl.aquila@gmail.com>
2022-02-08 23:15:00 -0300

    Add config for tailwind

---
+++ .history/tailwind.config_20220208231449.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+//ipfs-http-client is a decentralized way of hosting our URI (nft images/files) using promises to hook up to IPFS so that they'll be hosting their imges on with IPFS (IPFS is a distributed peer-to-peer hypermedia protocol).
+//hardhat can also initialize our solidity contracts
