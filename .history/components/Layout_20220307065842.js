b713cecab2813fde34b3f01e3863ce576a8d809b
realisticattorney <g.axl.aquila@gmail.com>
2022-03-07 07:03:48 -0300

    Fix body width

---
+++ .history/components/Layout_20220307065842.js
@@ -0,0 +1,19 @@
+import Nav from './Nav';
+import Web3Provider from './providers/web3';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <Web3Provider>
+      <div className="flex flex-col  sm:min-h-screen w-fit sm:w-full">
+        <Nav />
+        <main className="h-full w-full flex-grow appBackground">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </Web3Provider>
+  );
+};
+
+export default Layout;
